044233bbf97778d7205be659cdde606fdae17785
pratik-solanki-xduce <Pratik.Solanki@XDuce.com>
2025-07-08 11:51:35 +0000

    Bootstrap Integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { Cards } from "./Cards";
 import { FooterSection } from "./FooterSection";
@@ -5,18 +6,27 @@ import { HeaderSection } from "./HeaderSection";
 
 function App() {
   return (
-    <div className="main">
+    <div>
       <HeaderSection />
       <h1>Hello</h1>
-      <div className="row">
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-      </div>
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col lg="3" md="6">
+              <Cards />
+            </Col>
+            <Col lg="3" md="6">
+              <Cards />
+            </Col>
+            <Col lg="3" md="6">
+              <Cards />
+            </Col>
+            <Col lg="3" md="6">
+              <Cards />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
       <FooterSection />
     </div>
   );
