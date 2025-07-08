7dd4df1458bb0bfc5d837907f998d54ab6596596
pratik-solanki-xduce <Pratik.Solanki@XDuce.com>
2025-07-08 11:35:04 +0000

    Basic React

---
+++ src/FooterSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FooterSection = () => {
+  return (
+    <footer>
+      <h1>Footer Section</h1>
+    </footer>
+  );
+};
---
+++ src/HeaderSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HeaderSection = () => {
+  return (
+    <header>
+      <h1>Header Section</h1>
+    </header>
+  );
+};
